9c122c2f26967ea0ea3068cf20bbca79282bf612
huttona6 <huttona6@uni.coventry.ac.uk>
2021-10-07 11:19:10 +0100

    Started Selecting and Manipulating elements.

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number';
+
+document.querySelector('.score').textContent = '34';
