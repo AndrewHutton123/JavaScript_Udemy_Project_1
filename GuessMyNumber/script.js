4182bfa6c429a98ced9e69496e334b02a93aa7f9
huttona6 <huttona6@uni.coventry.ac.uk>
2021-10-07 11:43:37 +0100

    Handeling Click Events

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,7 +1,22 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number';
 
 document.querySelector('.score').textContent = '34';
+
+*/
+
+document.querySelector('.check').addEventListener
+('click',function(){
+    const guess = Number(document.querySelector(
+    '.guess').value);
+
+    console.log(guess);
+
+    if(!guess){
+        document.querySelector('.message').textContent = 'No Number';
+    }
+});
