6b3ea0c2229979e8bc3bae02abf5ac7737178cbe
huttona6 <huttona6@uni.coventry.ac.uk>
2021-10-11 14:00:08 +0100

    Added in Scoring

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -9,22 +9,53 @@ document.querySelector('.score').textContent = '34';
 
 */
 
-const secretNumber = Math.trunc(Math.random()*20)+1;
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+
+let score = 20;
+
 document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener
 ('click',function(){
     const guess = Number(document.querySelector(
     '.guess').value);
 
     console.log(guess);
-
+    //When there is no input
     if(!guess){
-        document.querySelector('.message').textContent = 'No Number';
+        document.querySelector('.message').textContent = 
+        'No Number';
+    //When Player Wins
     } else if (guess === secretNumber){
-        document.querySelector('.message').textContent = 'Correct Number';
+        document.querySelector('.message').textContent = 
+        'Correct Number';
+    //When Guess is too High
     } else if (guess > secretNumber){
-        document.querySelector('.message').textContent = 'Too High' 
+        if (score > 1  ){
+            document.querySelector('.message').textContent = 
+            'Too High'
+            score --;
+            document.querySelector('.score').textContent = 
+            score;
+        } else{
+            document.querySelector('.message').textContent = 
+            `You've Lost The Game`
+            document.querySelector('.score').textContent = 
+            0;
+        }
+    //When Guess is too Low
     }else if (guess < secretNumber){
-        document.querySelector('.message').textContent = 'Too Low' 
+        if (score > 1){
+            document.querySelector('.message').textContent = 
+        'Too Low' 
+        score --;
+        document.querySelector('.score').textContent = score;
+        }else{
+            document.querySelector('.message').textContent = 
+            `You've Lost The Game`
+            document.querySelector('.score').textContent = 
+            0;
+        }
+        
     }
 });
