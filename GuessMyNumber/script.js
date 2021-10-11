9b6dc0da883348c27bc1544e36916f4ad07fb036
huttona6 <huttona6@uni.coventry.ac.uk>
2021-10-11 12:16:30 +0100

    Begun Implementing Game Logic

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -9,6 +9,8 @@ document.querySelector('.score').textContent = '34';
 
 */
 
+const secretNumber = Math.trunc(Math.random()*20)+1;
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener
 ('click',function(){
     const guess = Number(document.querySelector(
@@ -18,5 +20,11 @@ document.querySelector('.check').addEventListener
 
     if(!guess){
         document.querySelector('.message').textContent = 'No Number';
+    } else if (guess === secretNumber){
+        document.querySelector('.message').textContent = 'Correct Number';
+    } else if (guess > secretNumber){
+        document.querySelector('.message').textContent = 'Too High' 
+    }else if (guess < secretNumber){
+        document.querySelector('.message').textContent = 'Too Low' 
     }
 });
